5f958f3488b28f8322ad89a9ec27d4ca0244cde3
Lewis <lewis@gmx.us>
2022-10-07 08:37:14 -0400

    command & event handler, added a few commands

--- main.js
+++ main.js
@@ -1,27 +1,38 @@
+const fs = require('node:fs');
+const path = require('node:path');
 const { token } = require('./config.json');
-const { Client, GatewayIntentBits } = require('discord.js');
-
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 
-client.once('ready', () => {
-    console.log('ready!');
-});
+// command handler
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
 
+// event handler
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const { commandName } = interaction;
-
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'beep') {
-		await interaction.reply('Boop!');
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
-});
+}
 
+client.on('interactionCreate', interaction => {
+	  
+});
 
 client.login(token);
